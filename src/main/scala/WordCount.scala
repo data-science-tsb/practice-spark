88439354a8b54cbffdec1b330a65a6d08b73e539
LM Bibera <hack.of.all.codes@gmail.com>
2017-01-22 19:38:39 +0800

    renamed to match other project

--- src/main/scala/WordCount.scala
+++ src/main/scala/WordCount.scala
@@ -1,14 +1,25 @@
+package com.sparkpractice
+
 import org.apache.spark.{SparkConf, SparkContext}
 
-object Main {
+object WordCount {
   def main(args: Array[String]) = {
-    val conf = new SparkConf().setAppName("spark-practice")
+    //acquire the spark context
+    val conf = new SparkConf().setAppName("word-count")
     val sc = new SparkContext(conf)
+
+    //load the input file
     val textFile = sc.textFile(args(0))
+
+    //perform count
     val counts = textFile.flatMap(line => line.split(" "))
       .map(word => (word, 1))
       .reduceByKey(_ + _)
+
+    //output the counts
     counts.saveAsTextFile(args(1))
+
+    //terminate context
     sc.stop()
   }
 }
